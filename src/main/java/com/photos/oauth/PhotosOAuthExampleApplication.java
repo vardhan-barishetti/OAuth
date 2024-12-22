8aa4f2b8eaa55bc066c0c9e987dbdb082b926f4f
vardhan barishetti <67987365+vardhan-barishetti@users.noreply.github.com>
2024-12-22 16:55:57 -0500

    Initial commit

---
+++ src/main/java/com/photos/oauth/PhotosOAuthExampleApplication.java
@@ -0,0 +1,13 @@
+package com.photos.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotosOAuthExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotosOAuthExampleApplication.class, args);
+	}
+
+}
